c7f937ab083f9177b169b86eced6d63adf9b95bd
bayan-97 <bayanalalem0@gmail.com>
2020-07-07 20:04:13 +0300

    lab3

--- lab2/js/scripts.js
+++ lab2/js/scripts.js
@@ -1,79 +1,131 @@
 'use strict'
-var ans1 =prompt( "what is your name?");
+var score=0;
+var ans1 = prompt('what is your name?');
 console.log(ans1);
-alert("welcome  "  + ans1 + "  lets to play  with each other ");
+alert('welcome  ' + ans1 + '  lets to play  with each other ');
 
-var ans2 =prompt( "is my fav drink coffee?");
+var ans2 = prompt('is my fav drink coffee?');
 console.log(ans2);
-alert(ans2);
 switch (ans2.toLocaleLowerCase()) {
-    case'yes':
-       alert("wow you can guess it");
-        break;
-    case'no':
-        alert("you cant guess it is coffee  ");
-        break;
+case 'yes':
+  alert('wow you can guess it');
+  score++;
+  break;
+case 'no':
+  alert('you cant guess it is coffee  ');
+  break;
 }
 
 
-var ans3 =prompt( "is my fav color red?");
+var ans3 = prompt('is my fav color red?');
 console.log(ans3);
-alert(ans3);
 
 switch (ans3.toLocaleLowerCase()) {
-    case 'yes':
-       alert("no it is purple dont give up");
-        break;
-    case 'no':
-        alert("great it is purple   ");
-        break;
-   
+case 'yes':
+  alert('no it is purple dont give up');
+  alert(ans3);
+  break;
+case 'no':
+  alert('great it is purple   ');
+  score++;
+  break;
+
 }
 
 
-var ans4 =prompt( "is my fav animale dog?");
+
+var ans4 = prompt('is my fav animale dog?');
 console.log(ans4);
-alert(ans4);
-
 switch (ans4.toLocaleLowerCase()) {
-    case 'yes':
-       alert("no it is cat we still have fun");
-        break;
-    case 'no':
-        alert(" it is unbeliveble it is sure cat ");
-        break;
-   
+case 'yes':
+  alert('no it is cat we still have fun');
+  break;
+case 'no':
+  alert(' it is unbeliveble it is sure cat ');
+  score++;
+  break;
+
 }
 
 
 
-var ans5=prompt( "is my fav food  pizza?");
+var ans5 = prompt('is my fav food  pizza?');
 console.log(ans5);
 switch (ans5.toLocaleLowerCase()) {
-    case 'yes':
-       alert("no it is spaghetti you  will know somthing else");
-        break;
-    case 'no':
-        alert(" it is unbeliveble it is sure cat ");
-        break;
-   
+case 'yes':
+  alert('no it is spaghetti you  will know somthing else');
+  break;
+case 'no':
+  alert(' it is unbeliveble it is sure cat ');
+  score++;
+  break;
+
 }
 
-var ans6=prompt( "is my fav sport  tennis?");
+var ans6 = prompt('is my fav sport  tennis?');
 console.log(ans6);
-alert(ans6);
 switch (ans5.toLocaleLowerCase()) {
-    case 'yes':
-       alert("yes really i am amazing from you");
-        break;
-    case 'no':
-        alert(" i love the tennis I ");
-        break;
-   
+case 'yes':
+  alert('yes really i am amazing from you');
+  score++;
+  break;
+case 'no':
+  alert(' i love the tennis I ');
+  break;
 }
 
-alert("i am so happy to know somthing about me" + "  nice to visit my web "+ ans1);
-alert("are you ready know funny secret about me??");
+alert('i am so happy to know somthing about me' + '  nice to visit my web ' + ans1);
+alert('are you ready know funny secret about me??');
+
+var g1=prompt( " how much i love cat from 10??");
+console.log( g1);
+if (g1===8) {
+  alert("it is right ");
+ score=score+1
+} else  { for (var  index = 0;(index < 4)&& g1!=8; index++) {
+ if ( (g1>=6 )) { 
+   alert("to high");
+    
+ } else if(g1<6) { alert("too low" );
+   
+ }
+ 
+ var g1=prompt( " how much i love cat from 10??");
+ console.log( g1);
+
+}
+while (g1===8) {
+  score=score+1
+ }
+alert( " the 8 its the right answer "  );
+}
+
+
+
+
+
+var q2=prompt( "do you know my favourits animals?");
+console.log( q2);
+var ar2 =["cat", "tigger"];
+if (q2===ar2[0].toLocaleLowerCase()||q2===ar2[1].toLocaleLowerCase()) {
+ alert("it is right") ;
+ alert("they are "+ar2[0]+ar2[1]);
+ score=score+1
+} else {for (var  i = 0; (i <6 && q2!==ar2[0]&& q2!==ar2[1]) ; i++) {
+var q2=prompt( "do you know my favourits animals?");
+}
+while (q2==ar2[0].toLocaleLowerCase() || q2 ==ar2[1].toLocaleLowerCase()) {
+  alert( "it is right ");
+  score=score+1
+  break;
+}
+alert("they are "+ar2[0]+ar2[1]);
+
+}
+alert("the score is"+ score);
+
+
+
 
 
 
