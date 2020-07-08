9ce8c4f5c06a7b3979b7071735ecc5c7bddbe1f3
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-07-08 16:14:31 +0300

    did the scretch goals

--- lab2/js/scripts.js
+++ lab2/js/scripts.js
@@ -9,85 +9,129 @@ function askForName() {
   alert("welcome  " + name + "  lets to play  with each other ");
 }
 function trueFalseAboutBayan() {
-  var favDrink = prompt("is my fav drink coffee?");
-  var ri = "yes";
-  console.log(favDrink);
-  answers.push(favDrink);
-  correctAnswers.push(ri);
-
-  switch (favDrink.toLocaleLowerCase()) {
-    case "yes":
-      alert("wow you can guess it");
+  var questions = [
+    {
+      body: "is my fav drink coffee?",
+      ri: "yes",
+      onYesMessage: "wow you can guess it",
+      onNoMessage: "you cant guess it is coffee",
+    },
+    {
+      body: "is my fav color red?",
+      ri: "no",
+      onYesMessage: "no it is purple dont give up",
+      onNoMessage: "great it is purple",
+    },
+    {
+      body: "is my fav animale dog?",
+      ri: "no",
+      onYesMessage: "No! its cat",
+      onNoMessage: "I prefer cats",
+    },
+    {
+      body: "is my fav food  pizza?",
+      ri: "no",
+      onYesMessage: "Who like pizza!?",
+      onNoMessage: "I perfer pasta",
+    },
+    {
+      body: "is my fav sport  tennis?",
+      ri: "yes",
+      onYesMessage: "Yess I really love it",
+      onNoMessage: "No I dont know other sport",
+    },
+  ];
+  console.log(questions);
+  for (let i = 0; i < questions.length; i++) {
+    var answer = prompt(questions[i].body, "yes");
+    if (answer === questions[i].ri) {
+      answers.push(answer);
+      alert(questions[i].onYesMessage);
       score++;
-      break;
-    case "no":
-      alert("you cant guess it is coffee  ");
-      break;
+    } else {
+      alert(questions[i].onNoMessage);
+    }
   }
 
-  var favColor = prompt("is my fav color red?");
-  console.log(favColor);
-  var ri2 = "no";
-  answers.push(favColor);
-  correctAnswers.push(ri2);
-  switch (favColor.toLocaleLowerCase()) {
-    case "yes":
-      alert("no it is purple dont give up");
-      break;
-    case "no":
-      alert("great it is purple   ");
-      score++;
-      break;
-  }
+  // var favDrink = prompt("is my fav drink coffee?");
+  // var ri = "yes";
+  // console.log(favDrink);
+  // answers.push(favDrink);
+  // correctAnswers.push(ri);
 
-  var likeDog = prompt("is my fav animale dog?");
-  console.log(likeDog);
-  var ri3 = "no";
-  answers.push(likeDog);
-  correctAnswers.push(ri3);
-  switch (likeDog.toLocaleLowerCase()) {
-    case "yes":
-      alert("no it is cat we still have fun");
-      break;
-    case "no":
-      alert(" it is unbeliveble it is sure cat ");
-      score++;
-      break;
-  }
+  // switch (favDrink.toLocaleLowerCase()) {
+  //   case "yes":
+  //     alert("wow you can guess it");
+  //     score++;
+  //     break;
+  //   case "no":
+  //     alert("you cant guess it is coffee  ");
+  //     break;
+  // }
 
-  var likePizza = prompt("is my fav food  pizza?");
-  console.log(likePizza);
-  var ri4 = "no";
-  answers.push(likePizza);
-  correctAnswers.push(ri4);
-  switch (likePizza.toLocaleLowerCase()) {
-    case "yes":
-      alert("no it is spaghetti you  will know somthing else");
-      break;
-    case "no":
-      alert(" it is unbeliveble it is sure cat ");
-      score++;
-      break;
-  }
+  // var favColor = prompt("is my fav color red?");
+  // console.log(favColor);
+  // var ri2 = "no";
+  // answers.push(favColor);
+  // correctAnswers.push(ri2);
+  // switch (favColor.toLocaleLowerCase()) {
+  //   case "yes":
+  //     alert("no it is purple dont give up");
+  //     break;
+  //   case "no":
+  //     alert("great it is purple   ");
+  //     score++;
+  //     break;
+  // }
 
-  var likeTennis = prompt("is my fav sport  tennis?");
-  console.log(likeTennis);
-  var ri5 = "yes";
-  answers.push(likeTennis);
-  correctAnswers.push(ri5);
-  switch (likeTennis.toLocaleLowerCase()) {
-    case "yes":
-      alert("yes really i am amazing from you");
-      score++;
-      break;
-    case "no":
-      alert(" i love the tennis I ");
-      break;
-  }
+  // var likeDog = prompt("is my fav animale dog?");
+  // console.log(likeDog);
+  // var ri3 = "no";
+  // answers.push(likeDog);
+  // correctAnswers.push(ri3);
+  // switch (likeDog.toLocaleLowerCase()) {
+  //   case "yes":
+  //     alert("no it is cat we still have fun");
+  //     break;
+  //   case "no":
+  //     alert(" it is unbeliveble it is sure cat ");
+  //     score++;
+  //     break;
+  // }
 
-  alert(
-    "i am so happy to know somthing about me" + "  nice to visit my web " + name
-  );
+  // var likePizza = prompt("is my fav food  pizza?");
+  // console.log(likePizza);
+  // var ri4 = "no";
+  // answers.push(likePizza);
+  // correctAnswers.push(ri4);
+  // switch (likePizza.toLocaleLowerCase()) {
+  //   case "yes":
+  //     alert("no it is spaghetti you  will know somthing else");
+  //     break;
+  //   case "no":
+  //     alert(" it is unbeliveble it is sure cat ");
+  //     score++;
+  //     break;
+  // }
+
+  // var likeTennis = prompt("is my fav sport  tennis?");
+  // console.log(likeTennis);
+  // var ri5 = "yes";
+  // answers.push(likeTennis);
+  // correctAnswers.push(ri5);
+  // switch (likeTennis.toLocaleLowerCase()) {
+  //   case "yes":
+  //     alert("yes really i am amazing from you");
+  //     score++;
+  //     break;
+  //   case "no":
+  //     alert(" i love the tennis I ");
+  //     break;
+  // }
+
+  // alert(
+  //   "i am so happy to know somthing about me" + "  nice to visit my web " + name
+  // );
 }
 
 console.log(answers);
@@ -147,5 +191,5 @@ askForName();
 trueFalseAboutBayan();
 catsMeter();
 favAnimals();
-alert("the score is" + score);
+alert(`your score is ${score}`);
 alert("are you ready know funny secret about me??");
