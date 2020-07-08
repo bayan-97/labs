e608700c90cb5dcee7eca0c9b46142134562528c
bayan-97 <bayanalalem0@gmail.com>
2020-07-08 13:13:50 +0300

    la

--- lab2/js/scripts.js
+++ lab2/js/scripts.js
@@ -1,11 +1,19 @@
 'use strict'
 var score=0;
+var arr1=[];
+var arr2=[];
+
+
 var ans1 = prompt('what is your name?');
 console.log(ans1);
 alert('welcome  ' + ans1 + '  lets to play  with each other ');
 
 var ans2 = prompt('is my fav drink coffee?');
+var ri="yes";
 console.log(ans2);
+arr1.push(ans2);
+arr2.push(ri);
+
 switch (ans2.toLocaleLowerCase()) {
 case 'yes':
   alert('wow you can guess it');
@@ -19,23 +27,27 @@ case 'no':
 
 var ans3 = prompt('is my fav color red?');
 console.log(ans3);
-
+var ri2="no"
+arr1.push(ans3);
+arr2.push(ri2);
 switch (ans3.toLocaleLowerCase()) {
 case 'yes':
   alert('no it is purple dont give up');
-  alert(ans3);
   break;
 case 'no':
   alert('great it is purple   ');
   score++;
   break;
 
+  
 }
-
 
 
 var ans4 = prompt('is my fav animale dog?');
 console.log(ans4);
+var ri3="no"
+arr1.push(ans4);
+arr2.push(ri3);
 switch (ans4.toLocaleLowerCase()) {
 case 'yes':
   alert('no it is cat we still have fun');
@@ -51,6 +63,9 @@ case 'no':
 
 var ans5 = prompt('is my fav food  pizza?');
 console.log(ans5);
+var ri4="no"
+arr1.push(ans5);
+arr2.push(ri4);
 switch (ans5.toLocaleLowerCase()) {
 case 'yes':
   alert('no it is spaghetti you  will know somthing else');
@@ -64,6 +79,9 @@ case 'no':
 
 var ans6 = prompt('is my fav sport  tennis?');
 console.log(ans6);
+ var ri5="yes"
+arr1.push(ans6);
+arr2.push(ri5);
 switch (ans5.toLocaleLowerCase()) {
 case 'yes':
   alert('yes really i am amazing from you');
@@ -76,6 +94,9 @@ case 'no':
 
 alert('i am so happy to know somthing about me' + '  nice to visit my web ' + ans1);
 alert('are you ready know funny secret about me??');
+console.log(arr1 );
+console.log(arr2 );
+
 
 var g1=prompt( " how much i love cat from 10??");
 console.log( g1);
@@ -123,6 +144,9 @@ alert("they are "+ar2[0]+ar2[1]);
 
 }
 alert("the score is"+ score);
+
+
+
 
 
 
