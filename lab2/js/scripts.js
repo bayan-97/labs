5a3efb7f299a6c0ece4016935a73dbae9d93b5ef
bayan-97 <bayanalalem0@gmail.com>
2020-07-10 23:20:05 +0300

    the end

--- lab2/js/scripts.js
+++ lab2/js/scripts.js
@@ -1,172 +1,97 @@
-"use strict";
+'use strict';
 var score = 0;
 var answers = [];
 var correctAnswers = [];
 
 function askForName() {
-  var name = prompt("what is your name?");
-  console.log(name);
-  alert("welcome  " + name + "  lets to play  with each other ");
+  var name = prompt('what is your name?');
+
+  alert('welcome  ' + name + '  lets to play  with each other ');
 }
 function trueFalseAboutBayan() {
   var questions = [
     {
-      body: "is my fav drink coffee?",
-      ri: "yes",
-      onYesMessage: "wow you can guess it",
-      onNoMessage: "you cant guess it is coffee",
+      body: 'is my fav drink coffee?',
+      ri: 'yes',
+      onYesMessage: 'wow you can guess it',
+      onNoMessage: 'you cant guess it is coffee',
     },
     {
-      body: "is my fav color red?",
-      ri: "no",
-      onYesMessage: "no it is purple dont give up",
-      onNoMessage: "great it is purple",
+      body: 'is my fav color red?',
+      ri: 'no',
+      onYesMessage: 'no it is purple dont give up',
+      onNoMessage: 'great it is purple',
     },
     {
-      body: "is my fav animale dog?",
-      ri: "no",
-      onYesMessage: "No! its cat",
-      onNoMessage: "I prefer cats",
+      body: 'is my fav animale dog?',
+      ri: 'no',
+      onYesMessage: 'No! its cat',
+      onNoMessage: 'I prefer cats',
     },
     {
-      body: "is my fav food  pizza?",
-      ri: "no",
-      onYesMessage: "Who like pizza!?",
-      onNoMessage: "I perfer pasta",
+      body: 'is my fav food  pizza?',
+      ri: 'no',
+      onYesMessage: 'Who like pizza!?',
+      onNoMessage: 'I perfer pasta',
     },
     {
-      body: "is my fav sport  tennis?",
-      ri: "yes",
-      onYesMessage: "Yess I really love it",
-      onNoMessage: "No I dont know other sport",
+      body: 'is my fav sport  tennis?',
+      ri: 'yes',
+      onYesMessage: 'Yess I really love it',
+      onNoMessage: 'No I dont know other sport',
     },
   ];
-  console.log(questions);
+
   for (let i = 0; i < questions.length; i++) {
-    var answer = prompt(questions[i].body, "yes");
+    var answer = prompt(questions[i].body, 'yes or no ');
     if (answer === questions[i].ri) {
       answers.push(answer);
       alert(questions[i].onYesMessage);
       score++;
     } else {
       alert(questions[i].onNoMessage);
+
+
     }
   }
 
-  // var favDrink = prompt("is my fav drink coffee?");
-  // var ri = "yes";
-  // console.log(favDrink);
-  // answers.push(favDrink);
-  // correctAnswers.push(ri);
-
-  // switch (favDrink.toLocaleLowerCase()) {
-  //   case "yes":
-  //     alert("wow you can guess it");
-  //     score++;
-  //     break;
-  //   case "no":
-  //     alert("you cant guess it is coffee  ");
-  //     break;
-  // }
-
-  // var favColor = prompt("is my fav color red?");
-  // console.log(favColor);
-  // var ri2 = "no";
-  // answers.push(favColor);
-  // correctAnswers.push(ri2);
-  // switch (favColor.toLocaleLowerCase()) {
-  //   case "yes":
-  //     alert("no it is purple dont give up");
-  //     break;
-  //   case "no":
-  //     alert("great it is purple   ");
-  //     score++;
-  //     break;
-  // }
-
-  // var likeDog = prompt("is my fav animale dog?");
-  // console.log(likeDog);
-  // var ri3 = "no";
-  // answers.push(likeDog);
-  // correctAnswers.push(ri3);
-  // switch (likeDog.toLocaleLowerCase()) {
-  //   case "yes":
-  //     alert("no it is cat we still have fun");
-  //     break;
-  //   case "no":
-  //     alert(" it is unbeliveble it is sure cat ");
-  //     score++;
-  //     break;
-  // }
-
-  // var likePizza = prompt("is my fav food  pizza?");
-  // console.log(likePizza);
-  // var ri4 = "no";
-  // answers.push(likePizza);
-  // correctAnswers.push(ri4);
-  // switch (likePizza.toLocaleLowerCase()) {
-  //   case "yes":
-  //     alert("no it is spaghetti you  will know somthing else");
-  //     break;
-  //   case "no":
-  //     alert(" it is unbeliveble it is sure cat ");
-  //     score++;
-  //     break;
-  // }
-
-  // var likeTennis = prompt("is my fav sport  tennis?");
-  // console.log(likeTennis);
-  // var ri5 = "yes";
-  // answers.push(likeTennis);
-  // correctAnswers.push(ri5);
-  // switch (likeTennis.toLocaleLowerCase()) {
-  //   case "yes":
-  //     alert("yes really i am amazing from you");
-  //     score++;
-  //     break;
-  //   case "no":
-  //     alert(" i love the tennis I ");
-  //     break;
-  // }
-
-  // alert(
-  //   "i am so happy to know somthing about me" + "  nice to visit my web " + name
-  // );
 }
 
-console.log(answers);
-console.log(correctAnswers);
+
 function catsMeter() {
-  var answer = prompt(" how much i love cat from 10??");
-  console.log(answer);
+  var answer = prompt(' how much i love cat from 10??');
+
   if (answer === 8) {
-    alert("it is right ");
+
+    alert('it is right ');
     score = score + 1;
   } else {
     for (var index = 0; index < 4 && answer != 8; index++) {
       if (answer >= 6) {
-        alert("too high");
+        alert('too high');
       } else if (answer < 6) {
-        alert("too low");
+        alert('too low');
       }
 
-      var answer = prompt(" how much i love cat from 10??");
+      var answer = prompt(' how much i love cat from 10??');
       console.log(answer);
     }
     if (answer === 8) {
+      console.log('it is right ');
       score++;
     }
-    alert(" the 8 its the right answer ");
+    alert(' the 8 its the right answer ');
   }
 }
 function favAnimals() {
-  var userAnswer = prompt("do you know my favourits animals?");
+  var userAnswer = prompt('do you know my favourits animals?');
   userAnswer = userAnswer.toLowerCase();
-  console.log(userAnswer);
-  var correctAnswers = ["cat", "tigger"];
+
+  var correctAnswers = ['cat', 'tigger'];
   if (userAnswer === correctAnswers[0] || userAnswer === correctAnswers[1]) {
-    alert("it is right");
-    alert("they are " + correctAnswers[0] + correctAnswers[1]);
+
+    alert('it is right');
+    alert('they are ' + correctAnswers[0] + correctAnswers[1]);
     score = score + 1;
   } else {
     for (
@@ -176,14 +101,17 @@ function favAnimals() {
       userAnswer !== correctAnswers[1];
       i++
     ) {
-      var userAnswer = prompt("do you know my favourits animals?");
+      // eslint-disable-next-line no-redeclare
+      var userAnswer = prompt('do you know my favourits animals?');
     }
+    // eslint-disable-next-line eqeqeq
     while (userAnswer == correctAnswers[0] || userAnswer == correctAnswers[1]) {
-      alert("it is right ");
+
+      alert('it is right ');
       score = score + 1;
       break;
     }
-    alert("they are " + correctAnswers[0] + correctAnswers[1]);
+    alert(`they are ${correctAnswers[0]} and ${correctAnswers[1]}`);
   }
 }
 
@@ -192,4 +120,4 @@ trueFalseAboutBayan();
 catsMeter();
 favAnimals();
 alert(`your score is ${score}`);
-alert("are you ready know funny secret about me??");
+alert('are you ready know funny secret about me?? scrol  down');
